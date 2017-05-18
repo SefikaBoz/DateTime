a9a31d10cf546cc359a796b401032c183243e688
SefikaBoz <sefikaboz12@gmail.com>
2017-05-18 17:12:04 +0300

    Create Time.cs

---
+++ Time.cs
@@ -0,0 +1,37 @@
+# DateTime
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateCalculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("uçuş tarihi (gün ay yıl) ve saati sırasıyla giriniz(saat dakika saniye)...");
+            int day = Convert.ToInt32(Console.ReadLine());
+            int month = Convert.ToInt32(Console.ReadLine());
+            int year = Convert.ToInt32(Console.ReadLine());
+            int hour = Convert.ToInt32(Console.ReadLine());
+            int minute = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Yolculuk suresi ne kadar ? (Gün , saat, dakika, saniye olarak sıralı girin..)");
+            int arrivalDay = Convert.ToInt32(Console.ReadLine());
+            int aHour = Convert.ToInt32(Console.ReadLine());
+            int aMinute = Convert.ToInt32(Console.ReadLine());
+            int aSecond = Convert.ToInt32(Console.ReadLine());
+
+            DateTime dateTimeObj = new DateTime(year, month, day, hour, minute, second);
+            Console.WriteLine(dateTimeObj);
+          
+            TimeSpan different = new TimeSpan(arrivalDay, aHour, aMinute, aSecond);
+            DateTime newDateTimeObj = dateTimeObj + different;
+
+            Console.WriteLine(newDateTimeObj);
